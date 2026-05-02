be71ff6d3e459a40c49bdaa16d594ed7e3324506
Chumus <vladiktimofeev50@gmail.com>
2026-05-02 19:16:34 +0300

    User requiers email instead of username and PostgreSQL connection

--- Zentro/settings.py
+++ Zentro/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Users'
 ]
 
 MIDDLEWARE = [
@@ -51,11 +52,17 @@ WSGI_APPLICATION = 'Zentro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST", default="localhost"),
+        'PORT': config("DB_PORT", default="5432")
     }
 }
 
+
+AUTH_USER_MODEL = "Users.User"
 
 AUTH_PASSWORD_VALIDATORS = [
     {
