8242d187e51e36f2d18e7f2a6ef6a2ea79f2d331
Chumus <vladiktimofeev50@gmail.com>
2026-05-02 22:21:21 +0300

    Added Homepage app and registration form html

--- HomePage/views.py
+++ HomePage/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View
 
 
-class SignUpView(View):
+class HomePageView(View):
 
     def get(self, request):
-        return render(request, "Users/SignUp.html")
+        return render(request, "HomePage/HomePage.html")
