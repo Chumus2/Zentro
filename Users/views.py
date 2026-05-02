0c6df078e288d651fae329ffbe9629e91a6fe0d1
Chumus <vladiktimofeev50@gmail.com>
2026-05-02 22:32:58 +0300

    Added sign up system

--- Users/views.py
+++ Users/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from .models import User
 
 
 class SignUpView(View):
 
     def get(self, request):
         return render(request, "Users/SignUp.html")
+    
+    def post(self, request):
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
+
+        if password != password2:
+            messages.error(request, "Password do not match")
+            return redirect("SignUp")
+        
+        elif User.objects.filter(email=email).exists():
+            messages.error(request, "User with this email already exists")
+            return redirect("SignUp")
+        
+        else:
+            user = User.objects.create(
+                username=username,
+                email=email,
+                password=password,
+            )
+            login(request, user)
+
+            return redirect("/")
+        
