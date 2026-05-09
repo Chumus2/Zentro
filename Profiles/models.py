5abfa7d3f549ffab23b529a3ed61598218f50dd6
Chumus <vladiktimofeev50@gmail.com>
2026-05-09 10:55:21 +0300

    Added bio field into profile model

--- Profiles/models.py
+++ Profiles/models.py
@@ -4,6 +4,7 @@ from django.conf import settings
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
+    bio = models.TextField(max_length=200, blank=True, null=True)
     icon = models.ImageField(upload_to="profile_icon/", blank=True, null=True)
     friends = models.ManyToManyField("self", blank=True)
 
