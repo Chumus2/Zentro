06caad0b296bcb3a9213d975ef7e697589f20518
Chumus <vladiktimofeev50@gmail.com>
2026-05-09 14:06:54 +0300

    Country select options + styles

--- Profiles/views.py
+++ Profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django_countries import countries
 from django.views import View
 from django.conf import settings
 from Users.models import User
@@ -12,4 +13,9 @@ class ProfilesView(LoginRequiredMixin, View):
     def get(self, request, user_id):
         user = get_object_or_404(User, id=user_id)
 
-        return render(request, "Profiles/Profiles.html", {"user": user})
+        context = {
+            "user": user,
+            "countries": countries
+        }
+
+        return render(request, "Profiles/Profiles.html", context)
