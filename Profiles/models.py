2f6bb0b72b54c04fdf21dd1916710b76a289df18
Chumus <vladiktimofeev50@gmail.com>
2026-05-09 11:54:28 +0300

    Added profile's stats (name, bio, country, birthday, icon

--- Profiles/models.py
+++ Profiles/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django_countries.fields import CountryField
 from django.conf import settings
 
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
     bio = models.TextField(max_length=200, blank=True, null=True)
-    birth_date = models.DateField(null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    country = CountryField(blank=True, null=True)
     icon = models.ImageField(upload_to="profile_icon/", blank=True, null=True)
     friends = models.ManyToManyField("self", blank=True)
 
