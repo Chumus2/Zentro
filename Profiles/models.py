5b153f14fa9ea271f5b19efad0544cbbe5884902
Chumus <vladiktimofeev50@gmail.com>
2026-05-09 10:58:50 +0300

    Added birth date field into profile model

--- Profiles/models.py
+++ Profiles/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
     bio = models.TextField(max_length=200, blank=True, null=True)
+    birth_date = models.DateField(null=True, blank=True)
     icon = models.ImageField(upload_to="profile_icon/", blank=True, null=True)
     friends = models.ManyToManyField("self", blank=True)
 
